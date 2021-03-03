7e12bef633a0bfa3ffe4bf8f10559106853dbf8a
Kareshma <kareshma@ntopology.com>
2021-03-03 12:05:29 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import pathlib
+import os
+
+publishPath = "//var//lib//jenkins//workspace//zzzz_upload_test//external"
+try:
+  print(f'Current Working dir path: {os.getcwd()}')
+  print(f'Publish path: {publishPath}')
+  os.chdir(publishPath)
+except Exception as error:
+  print(f'Current Working dir path: {os.getcwd()}')
+  print(f'Error occured: {error}')
+  return False
